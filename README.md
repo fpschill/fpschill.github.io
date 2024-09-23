ce7a7b230a945d18bdc5e393a13c3bb022194987
fpschill <scik@zhaw.ch>
2024-09-23 12:11:37 +0000

    Deploying to gh-pages from @ fpschill/fpschill.github.io@efec6c497438d3ebcfca8642beea1861c1bc62ee 🚀

---
+++ README.md
@@ -0,0 +1,3 @@
+# fpschill.github.io
+
+Repository of my personal homepage, hosted at https://fpschill.github.io
---
+++ merge_bib.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+rm -f ./_bibliography/web_papers.bib
+#cat  ./bibliography/pubs_zhaw.bib ./bibliography/pubs_journal.bib  ./bibliography/pubs_proc.bib ./bibliography/pubs_conf.bib ./bibliography/pubs_other.bib  ./_bibliography/papers.bib  >> ./_bibliography/web_papers.bib
+cat  ./bibliography/pubs_zhaw.bib ./bibliography/pubs_journal.bib  ./bibliography/pubs_proc.bib ./bibliography/pubs_conf.bib ./bibliography/pubs_other.bib  >> ./_bibliography/web_papers.bib
+
