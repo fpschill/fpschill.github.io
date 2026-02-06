f008b0833c4bd7993cd099ede4afe4e969098b5d
Frank-Peter Schilling <fpschill@gmail.com>
2026-02-06 14:07:16 +0100

    upd pubs

---
+++ CV/publications/make.ps1
@@ -0,0 +1,11 @@
+
+latex publications
+
+$numbers = 1..5
+foreach ($number in $numbers) {
+   bibtex publications$number
+}
+
+bibtex publications
+
+latex publications
--- CV/publications_old/readme.txt
+++
@@ -1,4 +0,0 @@
-myieeetran.bst:
-
-add DOI to ieeetran.bst, see
-https://tex.stackexchange.com/questions/67444/doi-in-ieeetran-bibliography
